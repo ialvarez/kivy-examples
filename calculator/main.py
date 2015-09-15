2398d6e925a44cb0d78be95e8816050a82479b68
Ignacio Alvarez <ignacio.alvarezbarrio@gmail.com>
2015-09-15 23:04:00 +0200

    first functionalities

--- calculator/main.py
+++ calculator/main.py
@@ -3,32 +3,47 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 
-
-class CalculatorNumbers(BoxLayout):
-    def __init__(self, **kwargs):
-        super(BoxLayout, self).__init__(**kwargs)
-        self.generate_numbers()
-
-    def generate_numbers(self):
-        numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
-        number_row = BoxLayout()
-        for number in numbers:
-            button = Button(text=str(number))
-            number_row.add_widget(button)
-        self.add_widget(number_row)
+from kivy.properties import ObjectProperty
 
 
 class CalculatorDisplay(Label):
     pass
 
 
+class CalculatorNumbers(BoxLayout):
+    def __init__(self, **kwargs):
+        super(CalculatorNumbers, self).__init__(**kwargs)
+        self.app = CalculatorApp.get_running_app()
+
+    def add_numbers(self):
+        numbers = [[7, 8, 9],
+                   [4, 5, 6],
+                   [1, 2, 3],
+                   [0]]
+
+        while(numbers):
+            numbers_row = BoxLayout()
+            for number in numbers.pop(0):
+                n = Button(text=str(number))
+                n.bind(on_press=self.add_to_display)
+                numbers_row.add_widget(n)
+            self.add_widget(numbers_row)
+
+    def add_to_display(self, instance):
+        self.app.calculator.ids.display.text += instance.text
+
+
 class CalculatorRoot(BoxLayout):
-    pass
+    def generate(self):
+        self.ids.numbers.add_numbers()
 
 
 class CalculatorApp(App):
+    calculator = ObjectProperty()
+
     def build(self):
-        calculator = CalculatorRoot()
-        return calculator
+        self.calculator = CalculatorRoot()
+        self.calculator.generate()
+        return self.calculator
 
 CalculatorApp().run()

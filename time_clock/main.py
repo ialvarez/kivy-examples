2ee351908a664b4446d706fe4f3b8518803109bc
Ignacio Alvarez <ignacio.alvarezbarrio@gmail.com>
2015-10-18 18:18:07 +0200

    some rework on time_clock

--- time_clock/main.py
+++ time_clock/main.py
@@ -9,13 +9,11 @@ from kivy.properties import StringProperty
 from kivy.adapters.listadapter import ListAdapter
 from kivy.clock import Clock
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 
-from sqlite_ex import TimeClockTable, Base
+from sqlite_ex import TimeClockTable, TimeClockEntries, Base
 from time_aux import TimeAux
-
-import time
 
 
 class TimeClockHeader(Label):
@@ -34,7 +32,7 @@ class TimeClockButton(Button):
     def __init__(self, **kwargs):
         super(TimeClockButton, self).__init__(**kwargs)
         self.app = TimeClockApp.get_running_app()
-        self.bind(on_press=self.add_time)
+        self.bind(on_press=self.app.on_tap)
 
     def add_time(self, instance):
         time_stamp = self.app.time_aux.time_stamp()
@@ -43,7 +41,8 @@ class TimeClockButton(Button):
 
 
 class TimeClockListItem(BoxLayout, ListItemLabel):
-    content = StringProperty()
+    ts_in = StringProperty()
+    ts_out = StringProperty()
 
 
 class TimeClockList(BoxLayout):
@@ -56,9 +55,10 @@ class TimeClockList(BoxLayout):
         self.generate_list_view()
 
     def roster_converter(self, index, value):
-        color = (0, 1, 0, 1) if value.action == 'in' else (1, 0, 0, 1)
+        color = (0, 1, 0, 1)  # if value.action == 'in' else (1, 0, 0, 1)
         return {
-            'content': value.time_stamp,
+            'ts_in': str(value.time_stamp_in),
+            'ts_out': str(value.time_stamp_out) if value.time_stamp_out else '',
             'color': color,
         }
 
@@ -84,6 +84,7 @@ class TimeClockApp(App):
     session = ObjectProperty()
     time_clock = ObjectProperty()
     time_aux = ObjectProperty()
+    last_entry = ObjectProperty()
 
     def build(self):
         engine = create_engine('sqlite:///time_clock.db')
@@ -92,6 +93,7 @@ class TimeClockApp(App):
         self.session = DBSession()
         self.time_aux = TimeAux()
         self.time_clock = TimeClockRoot()
+        self.last_entry = self.get_last_entry()
         Clock.schedule_interval(self.update, 1)
         return self.time_clock
 
@@ -99,7 +101,35 @@ class TimeClockApp(App):
         self.time_aux.update()
 
     def get_all(self):
-        return self.session.query(TimeClockTable).all()
+        if self.session.query(func.count(TimeClockEntries.id)).scalar() == 0:
+            return []
+        return self.session.query(TimeClockEntries).all()
+
+    def add(self, obj):
+        self.session.add(obj)
+        self.session.commit()
+
+    def add_entry(self):
+        entry = TimeClockEntries(time_stamp_in=None,
+                                 time_stamp_out=None)
+        self.add(entry)
+        return entry
+
+    def get_last_entry(self):
+        if self.session.query(func.count(TimeClockEntries.id)).scalar() == 0:
+            return None
+        return self.session.query(TimeClockEntries).order_by(TimeClockEntries.id.desc()).first()
+
+    def on_tap(self, instance):
+        if not self.last_entry or self.last_entry.time_stamp_out:
+            self.last_entry = self.add_entry()
+        time_stamp = self.time_aux.time_stamp()
+        if not self.last_entry.time_stamp_in:
+            self.last_entry.time_stamp_in = time_stamp
+        else:
+            self.last_entry.time_stamp_out = time_stamp
+        self.session.commit()
+        self.time_clock.ids.entries.generate_list_view()
 
     def add_new(self, time_stamp, action):
         self.session.add(TimeClockTable(time_stamp=time_stamp, action=action))
--- time_clock/sqlite_ex.py
+++ time_clock/sqlite_ex.py
@@ -1,9 +1,17 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy import create_engine
 
 
 Base = declarative_base()
+
+
+class TimeClockEntries(Base):
+    __tablename__ = 'entries'
+
+    id = Column(Integer, primary_key=True)
+    time_stamp_in = Column(Date)
+    time_stamp_out = Column(Date)
 
 
 class TimeClockTable(Base):
